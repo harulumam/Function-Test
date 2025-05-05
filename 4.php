79baaba71b7ee46a5e5621909b0ab3fcd0fe2b55
Khairul Umam <52909624+harulumam@users.noreply.github.com>
2025-05-05 21:38:22 +0700

    Nomor 4

---
+++ 4.php
@@ -0,0 +1,10 @@
+Kelebihan?
+- Teliti dan terstruktur
+- Cepat belajar dan terbuka pada teknologi baru
+- Tanggung jawab dan komitmen tinggi
+
+Kekurangan?
+- Perfeksionis pada hasil kerja
+- Belum memiliki pengalaman kerja hybrid/WFA
+
+Alasan saya adalah karena saya punya kemampuan teknis di web programming, terbiasa integrasi API, dan kerja secara terstruktur. Saya cepat belajar, bertanggung jawab, dan siap berkontribusi membangun sistem yang efisien dan bermanfaat.
